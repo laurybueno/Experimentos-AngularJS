091b5b32157bede66501c02b843051365c6a21e6
Laury Bueno <laury.bueno@gmail.com>
2016-07-03 20:11:46 -0300

    Melhoria no layout

--- scripts/primeiro-controller.js
+++ scripts/primeiro-controller.js
@@ -5,7 +5,9 @@ angular.module('aplicacao').controller('PrimeiroController', function($scope){
 
 	$scope.iniciado = true;
 
-	$scope.alunos = ['Aluno 1', 'Aluno 2', 'Aluno 3'];
+	$scope.alunos = [{'nome': 'Aluno 1', 'idade': 9},
+									{'nome':'Aluno 2', 'idade': 11},
+									{'nome':'Aluno 3', 'idade':15}];
 
 	$scope.finalizar = function(){
 		$scope.iniciado = false;
@@ -20,7 +22,10 @@ angular.module('aplicacao').controller('PrimeiroController', function($scope){
 
 	$scope.submeter = function(){
 		if($scope.form_1.$valid){
-			$scope.alunos.push($scope.nome_aluno);
+			novo_aluno = {};
+			novo_aluno['nome'] = $scope.nome_aluno;
+			novo_aluno['idade'] = parseInt($scope.idade_aluno);
+			$scope.alunos.push(novo_aluno);
 		} else {
 			alert('Preencha o nome corretamente.');
 		}
